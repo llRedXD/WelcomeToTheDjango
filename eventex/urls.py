ca09c7fdef4cfad3671da2f484982f5c87d821c2
llRedXD <gh.o.neira@hotmail.com>
2022-10-27 09:35:03 -0300

    feat: :sparkles: Tela de inscrição criada

--- eventex/urls.py
+++ eventex/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 import eventex.core.views
+from eventex.subscriptions.views import subscribe
 
 urlpatterns = [
+    path("inscricao/", subscribe),
     path('', eventex.core.views.home, name='home'),
     path('admin/', admin.site.urls),
 ]
